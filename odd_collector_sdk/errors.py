24b9c1046c35e58e1e2f7bf2317b15ea24965f28
Pavel Mackarichev <vixtir90@gmail.com>
2023-02-08 11:19:32 +0300

    feat: Add Collector's run method

--- odd_collector_sdk/errors.py
+++ odd_collector_sdk/errors.py
@@ -12,3 +12,8 @@ class DataSourceConnectionError(DataSourceError):
 
 class MappingDataError(Exception):
     pass
+
+
+class LoadConfigError(Exception):
+    def __init__(self, original_error, *args: object) -> None:
+        super().__init__(f"Couldn't handle config. Reason {original_error}", *args)
