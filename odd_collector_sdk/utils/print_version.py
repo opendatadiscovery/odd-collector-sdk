affb63dbec9d9b567893da585fc7d03d0c4847fa
Pavel Mackarichev <vixtir90@gmail.com>
2023-02-24 12:40:06 +0300

    feat: move logic to base Collector class

--- odd_collector_sdk/utils/print_version.py
+++ odd_collector_sdk/utils/print_version.py
@@ -7,7 +7,7 @@ from odd_collector_sdk.logger import logger
 def print_versions(*pkg_names: str):
     for pkg_name in pkg_names:
         try:
-            logger.info(f"{pkg_name}: {version(pkg_name)}")
+            logger.debug(f"{pkg_name}: {version(pkg_name)}")
         except Exception as e:
             logger.debug(traceback.format_exc())
             logger.warning(f"Could not get version for: {pkg_name}. Reason: {e}")
@@ -19,4 +19,4 @@ def print_collector_packages_info(collector_package: str) -> None:
     Args:
         collector_package (str): main collector package name
     """
-    print_versions(collector_package, "odd_models", "odd_collector_sdk")
+    print_versions(collector_package, "odd_collector_sdk", "odd_models")
