7b84c9f9d8712cbd4bfbd25e35ff37e15838dae3
Pavel Mackarichev <vixtir90@gmail.com>
2023-03-09 10:37:33 +0300

    chore: Added more verbose errors reason

--- odd_collector_sdk/api/http_client.py
+++ odd_collector_sdk/api/http_client.py
@@ -2,6 +2,8 @@ from typing import Any
 
 from aiohttp import ClientSession, ClientTimeout
 
+from odd_collector_sdk.errors import PlatformApiError
+
 
 class HttpClient:
     def __init__(self, token: str, connection_timeout_seconds: int = 300) -> None:
@@ -11,5 +13,13 @@ class HttpClient:
         }
         self.timeout = ClientTimeout(total=connection_timeout_seconds)
 
-    async def post(self, url: str, data: Any, session: ClientSession, **kwargs):
-        return await session.post(url=url, data=data, headers=self.headers, **kwargs)
+    async def post(self, url: str, data: Any, **kwargs):
+        async with ClientSession() as session:
+            response = await session.post(
+                url=url, data=data, headers=self.headers, **kwargs
+            )
+            if response.status >= 400:
+                body = await response.json()
+
+                raise PlatformApiError(body)
+            return response
