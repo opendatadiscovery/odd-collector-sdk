c86218be8641d468714f0ba603fa92d0ad2749c3
Pavel Makarichev <vixtir90@gmail.com>
2023-06-08 17:33:40 +0300

    feat: add filter (#27)

---
+++ odd_collector_sdk/domain/filter.py
@@ -0,0 +1,20 @@
+import re
+
+from funcy import partial
+from pydantic import BaseModel
+
+
+class Filter(BaseModel):
+    include: list[str] = []
+    exclude: list[str] = []
+    ignore_case: bool = False
+
+    def case_sensitive_flag(self):
+        return re.IGNORECASE if self.ignore_case else 0
+
+    def validate(self, value: str) -> bool:
+        match = partial(re.match, string=value, flags=self.case_sensitive_flag())
+        if any(match(pattern) for pattern in self.exclude):
+            return False
+
+        return any(match(pattern) for pattern in self.include)
